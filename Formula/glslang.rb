4f7cbdca1f1d312f9fdd7a1009d85a958034a6a3
Rafael Amador Galván <rafael.amador@gmail.com>
2020-07-19 15:39:25 -0500

    last change for MoltenVK builds

--- Formula/glslang.rb
+++ Formula/glslang.rb
@@ -37,13 +37,6 @@ class Glslang < Formula
       "-DENABLE_GLSLANG_BINARIES=ON",
     ]
 
-    inreplace Dir["#{buildpath}/CMakeLists.txt"].each do |s|
-      s.gsub! "pkg_check_modules(SPIRV_TOOLS REQUIRED SPIRV-Tools>=2019.2.1)", \
-          "pkg_check_modules(SPIRV_TOOLS REQUIRED SPIRV-Tools>=2019.2.0)"
-      s.gsub! "pkg_check_modules(SPIRV_TOOLS_SHARED REQUIRED SPIRV-Tools-shared>=2019.2.1)", \
-          "pkg_check_modules(SPIRV_TOOLS_SHARED REQUIRED SPIRV-Tools-shared>=2019.2.0)"
-    end
-
     mkdir "build" do
       system "cmake", "-G", "Ninja", "..", *args
       system "ninja"
